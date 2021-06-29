75df9e20ccab4061cacc295eff1b44e687a7565d
이승재 <lsj8367@naver.com>
2021-06-29 13:03:40 +0900

    remove: User객체 관련 파일 삭제

--- src/main/java/com/lsj/board/domain/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lsj.board.domain.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
--- src/main/java/com/lsj/board/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.lsj.board.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-
-}
