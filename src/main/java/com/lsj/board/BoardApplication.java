407cd7b226a3a03a485ca0515366732f474fb7b1
SeungJae Lee <lsj8367@naver.com>
2021-08-15 18:10:00 +0900

    fix: Jpa Auditing 설정 변경

--- src/main/java/com/lsj/board/BoardApplication.java
+++ src/main/java/com/lsj/board/BoardApplication.java
@@ -2,14 +2,10 @@ package com.lsj.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //LocalDateTime 자동삽입
 @SpringBootApplication
 public class BoardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/lsj/board/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.lsj.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
